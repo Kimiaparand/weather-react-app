413d58633d10439f612999c3fe856681158758a9
Kimiaparand <120417079+Kimiaparand@users.noreply.github.com>
2023-05-09 21:17:28 +0430

    Added URL github

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <p className="githup">
+          This project was codedd by Kimia Fatehi is opened-source on{" "}
+          <a href="https://github.com/Kimiaparand/weather-react-app">Github</a>
+        </p>
       </header>
     </div>
   );
